f6bf11c04c73670798c9f4d5bb9dac522e6bff6b
Anzelo7 <andrea.anzellotti7@gmail.com>
2022-09-26 16:58:51 +0200

    push comics

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <HeaderComponent />
+    
+    <MainComponent :cards="comics" />
+    <FooterComponent />
+  </div>
+</template>
+
+<script>
+import HeaderComponent from './components/HeaderComponent.vue';
+import MainComponent from './components/MainComponent.vue';
+import FooterComponent from './components/FooterComponent.vue';
+// import { comics } from '@/assets/data/dc-comics';
+import comics from '@/assets/data/dc-comics.json'
+//console.log(comics);
+
+export default {
+  name: 'App',
+  components: {
+    HeaderComponent,
+    MainComponent,
+    FooterComponent,
+  },
+  data(){
+    return {
+      comics
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+@import './styles/generic';
+@import './styles/variables';
+.container{
+  margin: 0 auto;
+  max-width: 1200px;
+}
+
+
+</style>
---
+++ src/components/MainComponent.vue
@@ -0,0 +1,45 @@
+<template>
+  <main>
+    <div class="container">
+      <div class="container-item" v-for="card in cards" :key="card.series">
+        <CardComponent :card="card" />
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+import CardComponent from '@/components/CardComponent.vue';
+
+export default {
+  name: 'MainComponent',
+  props: {
+    cards: Array,
+  },
+  // props: ['cards'] // per prototipare
+  components: {
+    CardComponent,
+  },
+  mounted() {
+    console.log(this.cards);
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import '@/styles/variables';
+
+.container {
+  display: flex;
+  flex-wrap: wrap;
+}
+.container-item {
+  width: calc(100% / 6);
+}
+
+main {
+  background-color: $bg-main;
+  color: $color-main;
+  padding: 50px 0px;
+}
+</style>
